db1e6ab448ac750311f82440a5a9438a2a86a34d
Bradley Biladeau <bradley.biladeau@allata.com>
2018-10-23 16:49:30 -0600

    addition of tests

--- Test/W9000.Test/UnitTest1.cs
+++ Test/W9000.Test/UnitTest1.cs
@@ -1,7 +1,42 @@
 using System;
+using Moq;
+using W9000.Business;
+using W9000.Data;
+using W9000.Entities;
 using Xunit;
 
 namespace W9000.Test
 {
-	
+	public class FillOrderServiceTest
+	{
+		[Fact]
+		public void ProcessFillOrder_On_Open_Order()
+		{
+			Mock<IFillOrderRepo> fillOrderRepo = new Mock<IFillOrderRepo>();
+			FillOrderService fillOrderService = new FillOrderService(fillOrderRepo.Object);
+			var newOrder = fillOrderService.CreateFillOrder();
+			Assert.IsType<DateTime>(newOrder.OrderCreated);
+		}
+
+		[Fact]
+
+		public void ProcessFillOrder_On_Closed_Order()
+		{
+			Mock<IFillOrderRepo> fillOrderRepo = new Mock<IFillOrderRepo>();
+			FillOrder order = new FillOrder();
+			order.OrderCreated = DateTime.Now;
+			order.OrderId = Guid.NewGuid();
+			FillOrderService fillOrderService = new FillOrderService(fillOrderRepo.Object);
+			var closedOrder = fillOrderService.ProcessFillOrder(order);
+			Assert.True(closedOrder.OrderClosed);
+		}
+
+		[Fact]
+		public void ViewOpenOrders_Shows_All_Open_Orders()
+		{
+			Mock<IFillOrderRepo> fillOrderRepo = new Mock<IFillOrderRepo>();
+			FillOrderService fillOrderService = new FillOrderService(fillOrderRepo.Object);
+			var newOrder = fillOrderService.CreateFillOrder();
+		}
+	}
 }
